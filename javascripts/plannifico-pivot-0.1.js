1517e934077bad2dfa1eea00c628be3ad00db6d1
Plannifico <plannifico.org@gmail.com>
2014-09-20 10:28:26 +0200

    added filter management

--- javascripts/plannifico-pivot-0.1.js
+++ javascripts/plannifico-pivot-0.1.js
@@ -14,13 +14,20 @@ limitations under the License.*/
 
 /*
 - i_container: a JQuery <DIV> element containing the pivot table
+
 - i_configuration: configuration parameters in the following format:
+
 - i_on_change_query: the function that must be called every time the layout of the pivot change in order to get the new data with the following parameters:
 	* q_dimensions: an array containing the dimensions to show in the layout
 	* q_measures: an array containing the measure to show in the layout
 	* q_filters: an array of object in the format {dimension: value} in order to filter the dataset
+
+- i_get_dim_attribute_elements: the function that must be called every time a dimension attribute elements list is needed:
+
+	* dimension.attribute: the dimension attribute the list must be returned (<dimension_name>.<attribute_name>)
+	
 */
-function PlannificoPivot (i_container, i_configuration, i_on_change_query) {
+function PlannificoPivot (i_container, i_configuration, i_on_change_query, i_get_dim_attribute_elements) {
 
 	this.container = i_container;
 	this.configuration = i_configuration;
@@ -36,6 +43,13 @@ function PlannificoPivot (i_container, i_configuration, i_on_change_query) {
 	} else 
 		this.onChangeQuery = function (d,m,f) {console.log ("this.onChangeQuery not set");};
 	
+	if (i_get_dim_attribute_elements && (typeof i_get_dim_attribute_elements == "function")) {
+		
+		this.getDimAttributeElements = i_get_dim_attribute_elements;		
+		
+	} else 
+		this.getDimAttributeElements = function (d,a) {console.log ("this.getDimAttributeElements not set"); return []};
+
 }
 
 /*
@@ -61,9 +75,11 @@ PlannificoPivot.prototype.refresh = function () {
 		"class": 'span-20'
 			
 	}).appendTo (this.container);
-	
+		
 	this.addDraggableDimensions (this.configuration.dimensionsLabel, this.configuration.dimensions, "dim-", "draggable-dim");
+
 	this.addDraggableMeasures (this.configuration.measuresLabel, this.configuration.measures, "measure-", "draggable-measure");
+
 	this.addPivotArea ();
 	
 	this.applyDroppable ();
@@ -71,6 +87,10 @@ PlannificoPivot.prototype.refresh = function () {
 
 /*Private methods*/
 
+/*
+	Add the Pivot Area built by the selection area (where dimensions and measures are dropped) 
+	and the data area where the result of the selection is diplayed
+*/
 PlannificoPivot.prototype.addPivotArea = function () {	
 
 	var pivot_area = $ ("<div>", {
@@ -91,11 +111,26 @@ PlannificoPivot.prototype.addPivotArea = function () {
 		"class": 'span-20'
 			
 	}).appendTo (pivot_selection).html (this.configuration.selectionHeaderLabel);
+
+	this.createPivotSelection (pivot_selection);
 	
+	var pivot_data = $ ("<div>", {
+		
+		"id": 'pivot-data',
+		"class": 'span-20'
+			
+	}).appendTo (pivot_area).html (this.configuration.emptySelectionLabel.dataArea);
+}	
+
+/*
+	Add the Pivot Selection area
+*/
+PlannificoPivot.prototype.createPivotSelection = function (pivot_selection) {	
+
 	var pivot_selection_rows = $ ("<div>", {
 		
 		"id": 'pivot-selection-rows',
-		"class": 'span-6 border ui-widget-header droppable-dim'
+		"class": 'span-4 border ui-widget-header droppable-dim'
 			
 	}).appendTo (pivot_selection).html (this.configuration.emptySelectionLabel.rows);
 	
@@ -116,17 +151,11 @@ PlannificoPivot.prototype.addPivotArea = function () {
 	var pivot_selection_filters = $ ("<div>", {
 		
 		"id": 'pivot-selection-filters',
-		"class": 'span-4 border ui-widget-header droppable-dim'
+		"class": 'span-6 border ui-widget-header droppable-filter'
 			
 	}).appendTo (pivot_selection).html (this.configuration.emptySelectionLabel.filters);
-	
-	var pivot_data = $ ("<div>", {
-		
-		"id": 'pivot-data',
-		"class": 'span-20'
-			
-	}).appendTo (pivot_area).html (this.configuration.emptySelectionLabel.dataArea);
-}	
+
+}
 
 PlannificoPivot.prototype.addAttributeSelection = function (dims_container, id_prefix, additional_classes, dimensions, dimension) {
 
@@ -236,6 +265,9 @@ PlannificoPivot.prototype.addDraggableMeasures = function (label, elements, id_p
 	$( "#measure-menu" ).menu();
 }
 
+/*
+	Apply the droppable behavior to the componet
+*/
 PlannificoPivot.prototype.applyDroppable = function () {	
 
 	var self = this;
@@ -253,12 +285,23 @@ PlannificoPivot.prototype.applyDroppable = function () {
 		scope: ".pivot-selection-measure",
 		cursor: 'move'
 	});
-	
+
+	this.applyDroppableDim ();
+	this.applyDroppableMeasures ();
+	this.applyDroppableFilters ();
+}	
+
+/*
+	Apply the droppable behavior to the dimensions selection box
+*/
+PlannificoPivot.prototype.applyDroppableDim = function () {	
+
 	$(".droppable-dim").droppable({
 		
 		scope: ".pivot-selection-dim",
 		
 		greedy: "true",
+
 		
 		drop: function( event, ui ) {
 			
@@ -315,6 +358,13 @@ PlannificoPivot.prototype.applyDroppable = function () {
 		}
 	});
 
+}
+
+/*
+	Apply the droppable behavior to the measures selection box
+*/
+PlannificoPivot.prototype.applyDroppableMeasures = function () {	
+
 	$(".droppable-measure").droppable({
 		
 		scope: ".pivot-selection-measure",
@@ -353,5 +403,95 @@ PlannificoPivot.prototype.applyDroppable = function () {
 			
 			//$( this ).addClass( "ui-state-highlight" ).find( "p" ).html( "Dropped!" );
 		}
-	});	
-}	
+	});
+}
+
+/*
+	Apply the droppable behavior to the dimensions selection box
+*/
+PlannificoPivot.prototype.applyDroppableFilters = function () {	
+	
+	var self = this;
+
+	$(".droppable-filter").droppable({
+		
+		scope: ".pivot-selection-dim",
+		
+		greedy: "true",
+		
+		drop: function( event, ui ) {
+			
+			console.log ("dropped " + $(ui.draggable).attr ("pl-label"));
+			
+			var dropped_dim_id = $(ui.draggable).attr ("id").replace ("droppable-","");
+			
+			var dropTarget = $(this);
+
+			var dropped_dim = $('<div>', {"id": 'dropped-' + dropped_dim_id,"class": 'span-5'}).appendTo (dropTarget);
+			
+			var dropped_dim_lb = $('<div>', {"class": 'span-3 border'}).appendTo (dropped_dim).html($(ui.draggable).attr ("pl-label"));
+			
+			var comparison_sign = $('<div>', {"class": 'span-2 border'}).appendTo (dropped_dim).html ("=");
+
+			var filter_selection = $('<select>', 
+
+				{"id": 'filter-selection', "class": 'span-3 border'}
+
+			).appendTo (dropped_dim).html("select element");
+
+			var dimension = $(ui.draggable).attr ("pl-label").split(".")[0];
+			var attribute = $(ui.draggable).attr ("pl-label").split(".")[1];
+
+			var filter_elements = self.getDimAttributeElements (dimension, attribute);
+
+			$.each (filter_elements,function (index,element) {
+
+				filter_selection.append(new Option(element.text, element.value));
+			});
+
+			var dropped_dim_del_btn = $('<div>', 
+
+				{"id": 'delbtn-' + dropped_dim_id, "class": 'span-1'}
+
+			).appendTo (dropped_dim).html("x");
+			
+			$("#delbtn-" + dropped_dim_id).click (function() {
+		
+				console.log("$(this).id " + $(this).attr ("id"));
+				
+				var dropped_id = $(this).attr ("id").replace ("delbtn-","");				
+				
+
+				$("#dropped-" + dropped_id).remove();
+		
+				$("#" + $(this).attr("id") + "_attribute_sel").toggle();
+				
+				$("#droppable-" + dropped_id).show();
+
+				console.log ("parent-id " + $("#droppable-" + dropped_id).attr ("parent-id"));
+
+				$("#droppable-" + dropped_id).show();
+			});
+			
+			$(ui.draggable).hide();
+			
+			console.log ("dropTarget.id " + $(dropTarget).attr ("id"));
+			
+			var target_id = $(dropTarget).attr ("id");
+			
+			if ((target_id == "pivot-selection-rows") || (target_id == "pivot-selection-cols")) {
+				
+				console.log ("dataNavigationInvoker.groupby " + $(ui.draggable).attr("id"));
+				
+				self.currentDimensions.push ($(ui.draggable).text());
+				
+				self.onChangeQuery (self.currentDimensions,self.currentMeasures,self.currentFilters);
+				
+				//dataNavigationInvoker.groupby.push ($(ui.draggable).text());					
+			}
+		}
+	});
+
+}
+
+
